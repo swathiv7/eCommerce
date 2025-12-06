6a087347a26937d1654e35529c16dd58a5b131a3
swathi_v7 <swathinarmgowda@gmail.com>
2025-12-06 13:27:33 +0300

    Adding and fetching categories logic added

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,19 +1,31 @@
 package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.service.CategoryService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class CategoryController {
 
-    private List<Category> categories = new ArrayList<>();
+    private CategoryService categoriesService;
+
+    public CategoryController(CategoryService categories) {
+        this.categoriesService = categories;
+    }
 
     @GetMapping("/api/public/categories")
-    public List<Category> getsAllCategories(){
-        return categories;
+    public List<Category> getAllCategories(){
+        return categoriesService.getAllCategories();
+    }
+
+    @PostMapping("/api/public/categories")
+    public String addCategories(@RequestBody Category category){
+        categoriesService.addCategories(category);
+        return "Successfully added the category";
     }
 }
---
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -0,0 +1,11 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.model.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    public List<Category> getAllCategories();
+    void addCategories(Category category);
+}
